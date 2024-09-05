5f023d48fa699c476c41f369287400f5289033b0
Delgermurun17 <88delgeree@gmail.com>
2024-09-05 15:30:12 +0800

    aaa

--- components/RecordsAdd.jsx
+++ components/RecordsAdd.jsx
@@ -17,7 +17,7 @@ export function RecordsAdd() {
   const expense = "EXPENSE";
   const income = "INCOME";
   const router = useRouter()
-  const searchParams = useSearchParams("/s")
+  const searchParams = useSearchParams("/")
   const record = searchParams.get('record')
   const open = record === 'add'
 
